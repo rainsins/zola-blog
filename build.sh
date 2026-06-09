a4b2b0b61aca7fecb60fbc475dd13979518eeeac
rainsins <1820278582@qq.com>
2026-06-09 23:18:58 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
+
+set -euo pipefail
+
 main() {
     ZOLA_VERSION=0.22.1
 
     curl -sLJO "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-    tar -xf zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+    tar -xf "zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
 
     git submodule update --init --recursive
 
     ./zola build
 }
 
-set -euo pipefail
+main "$@"   # ← 这行是关键
